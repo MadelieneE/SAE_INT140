1b4432a9ccb152cf8084eac2bbc576c8cd129f5c
R Haubt <info@roberthaubt.com>
2018-10-10 15:26:09 +1100

    Week 4 Lecture Files
    this form send info to the checkout file

---
+++ form.php
@@ -0,0 +1,20 @@
+<form action='checkout.php' method='post'>
+<input type='hidden' name='op' value='submitSpecial'>
+<table align='center'>
+	<tr>
+		<td>Body:</td>
+		<td><input type='text' name='body' size='40' length='100' value='<?php echo $_POST['body']; ?>'></td>
+	</tr>
+	<tr>
+		<td>Start Date:</td>
+		<td><input type='text' name='startDate' size='10' value='<?php echo $_POST['startDate']; ?>'></td>
+	</tr>
+	<tr>
+		<td>End Date:</td>
+		<td><input type='text' name='endDate' size='10' value='<?php echo $_POST['endDate']; ?>'></td>
+	</tr>
+	<tr>
+		<td align='center' colspan='3'><input type='submit' name='submitSpecial' value='Submit'></td>
+	</tr>
+</table>
+</form>
