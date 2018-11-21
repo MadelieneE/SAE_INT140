74d79e2c79d5b29b9ae3f5804a35e8a3bf4a3331
R Haubt <info@roberthaubt.com>
2018-11-21 15:13:44 +1100

    Update form.php

--- form.php
+++ form.php
@@ -3,15 +3,15 @@
 <table align='center'>
 	<tr>
 		<td>Body:</td>
-		<td><input type='text' name='body' size='40' length='100' value='<?php echo $_POST['body']; ?>'></td>
+		<td><input type='text' name='body' size='40' length='100'></td>
 	</tr>
 	<tr>
 		<td>Start Date:</td>
-		<td><input type='text' name='startDate' size='10' value='<?php echo $_POST['startDate']; ?>'></td>
+		<td><input type='text' name='startDate' size='10'></td>
 	</tr>
 	<tr>
 		<td>End Date:</td>
-		<td><input type='text' name='endDate' size='10' value='<?php echo $_POST['endDate']; ?>'></td>
+		<td><input type='text' name='endDate' size='10'></td>
 	</tr>
 	<tr>
 		<td align='center' colspan='3'><input type='submit' name='submitSpecial' value='Submit'></td>
