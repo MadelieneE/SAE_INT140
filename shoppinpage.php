5e0fe5fc982a4831b381aa3dbf4051d64d4e6cad
R Haubt <info@roberthaubt.com>
2018-11-28 14:17:50 +1100

    Create shoppinpage.php

---
+++ shoppinpage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Radio Buttons</h2>
+
+<form>
+  <input type="radio" name="gender" value="male" checked> <img style=width:150px;height:150px;" src="https://pumaimages.azureedge.net/images/191592/01/sv01/fnd/PNA/h/600/w/600"><br>
+  <input type="radio" name="gender" value="female"> <img style=width:150px;height:150px;" src="https://pumaimages.azureedge.net/images/191592/01/sv01/fnd/PNA/h/600/w/600"><br>
+  <input type="radio" name="gender" value="other"> <img style=width:150px;height:150px;" src="https://pumaimages.azureedge.net/images/191592/01/sv01/fnd/PNA/h/600/w/600">  
+</form> 
+
+</body>
+</html>
