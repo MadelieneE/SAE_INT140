3639a51c9cfdf50f996bba74656d8d7654cf8ac9
R Haubt <info@roberthaubt.com>
2018-11-28 14:21:44 +1100

    Create shoppinpage_checkboxes.php

---
+++ shoppinpage_checkboxes.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Radio Buttons</h2>
+
+<form>
+  <input type="checkbox" name="gender" value="male" checked> <img style=width:150px;height:150px;" src="https://pumaimages.azureedge.net/images/191592/01/sv01/fnd/PNA/h/600/w/600"><br>
+  <input type="checkbox" name="gender" value="female"> <img style=width:150px;height:150px;" src="https://pumaimages.azureedge.net/images/191592/01/sv01/fnd/PNA/h/600/w/600"><br>
+  <input type="checkbox" name="gender" value="other"> <img style=width:150px;height:150px;" src="https://pumaimages.azureedge.net/images/191592/01/sv01/fnd/PNA/h/600/w/600">  
+</form> 
+
+</body>
+</html>
